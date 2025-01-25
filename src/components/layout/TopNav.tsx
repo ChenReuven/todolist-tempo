6e729c7eb2b780167aad79ffaed3bb5a2068a075
ChenReuven <chen.rvn@gmail.com>
2025-01-25 06:26:33 +0000

    remove "chen" label

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -25,7 +25,6 @@ const TopNav = ({
 }: TopNavProps) => {
   return (
     <div className="w-full h-16 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 px-4 flex items-center justify-between">
-      <div>Chen</div>
       {/* Search Bar */}
       <div className="flex-1 max-w-md">
         <div className="relative">
